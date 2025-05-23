fb24a022c73f5ccd95c46f7fd4d3812b07d97f42
Son Roy Almerol <sralmerol@sgl.com>
2025-05-23 12:35:28 -0400

    fix description json tag

--- internal/store/types/scripts.go
+++ internal/store/types/scripts.go
@@ -2,7 +2,7 @@ package types
 
 type Script struct {
 	Path        string `config:"type=string,required" json:"path"`
-	Description string `json:"name"`
+	Description string `json:"description"`
 	JobCount    int    `json:"job_count"`
 	TargetCount int    `json:"target_count"`
 	Script      string `json:"script"`
